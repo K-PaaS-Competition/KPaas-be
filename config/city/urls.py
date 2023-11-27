6697c9b50deb20aa4888e784f19361ebbc1d1a0a
kym8821 <louie9798@naver.com>
2023-11-28 02:05:51 +0900

    [feat] cityDataAll[get], subRegionData[get]

--- config/city/urls.py
+++ config/city/urls.py
@@ -3,5 +3,8 @@ from city.views import *
 
 urlpatterns = [
     path("get/", CityData.as_view()),
+    path("getAll/", CityDataAll.as_view()),
     path("load/", LoadData.as_view()),
+    path("subRegion/all/", SubRegionData.as_view()),
+    path("subRegion/city/", SubRegionDataByCity.as_view()),
 ]
---
+++ config/city/views/subRegionData.py
@@ -0,0 +1,42 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from city.models import SubRegion, City
+
+
+class SubRegionData(APIView):
+    # 전부 반환
+    def get(self, request, *args, **kwargs):
+        data = SubRegion.objects.all()
+        return Response(data.values())
+
+
+class SubRegionDataByCity(APIView):
+    def get(self, request, *args, **kwargs):
+        try:
+            cityName = request.GET["city"]
+            city = City.objects.filter(name=cityName)
+            if city.exists():
+                subRegion = SubRegion.objects.filter(city=city.first())
+                print(subRegion.values())
+                return Response(
+                    {
+                        "data": subRegion.values(),
+                        "status": 200,
+                    }
+                )
+            return Response(
+                {
+                    "data": None,
+                    "message": "Data Not Exist",
+                    "status": 400,
+                }
+            )
+        except KeyError:
+            return Response(
+                {
+                    "data": None,
+                    "message": "KeyError",
+                    "status": 400,
+                }
+            )
