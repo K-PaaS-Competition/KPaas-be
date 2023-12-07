4c239e6a7a5c5c01a66f51efe34d98890954d229
kym8821 <louie9798@naver.com>
2023-12-07 19:13:07 +0900

    cors

--- config/config/settings/prod.py
+++ config/config/settings/prod.py
@@ -2,3 +2,10 @@ from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = ["49.50.164.200"]
+
+# https://dev--floodingpoint-seoul.netlify.app/
+CORS_ORIGIN_WHITELIST = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "https://dev--floodingpoint-seoul.netlify.app/",
+]
